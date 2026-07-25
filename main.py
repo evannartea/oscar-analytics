f284a8ceefe74c1d9c5aaeaec65557ca630e8623
Evan Nartea <evannartea@Mac.lan>
2026-07-25 17:46:32 +0100

    added postgres database

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
 import pandas as pd
-from functions.elt import extract_data
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from functions.elt import extract_data, export_to_db
 
 def main():
+    load_dotenv()
     oscar_data = extract_data()
+    database_url = os.getenv("DATABASE_URL")
+    engine = create_engine(database_url)
+
     df = pd.DataFrame(oscar_data)
 
     print(len(df))
