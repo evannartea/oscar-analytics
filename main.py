b1b50817523ba0ad87fcf390d99c6049dc523d92
Evan Nartea <evannartea@Mac.lan>
2026-07-25 18:11:08 +0100

    added setup

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
 import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from functions.elt import extract_data, export_to_db
 
 def main():
+    load_dotenv()
+    database_url = os.getenv("DATABASE_URL")
+    engine = create_engine(database_url)
+
     df_oscars = extract_data()
-    export_to_db("oscars_history", df_oscars)
 
     print(len(df_oscars))
 
