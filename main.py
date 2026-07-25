b2deb30161c8007a1719035e1af2b3450860931e
Evan Nartea <evannartea@Mac.lan>
2026-07-25 18:02:51 +0100

    moved functions

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-import os
 import pandas as pd
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
 from functions.elt import extract_data, export_to_db
 
 def main():
-    load_dotenv()
-    oscar_data = extract_data()
-    database_url = os.getenv("DATABASE_URL")
-    engine = create_engine(database_url)
+    df_oscars = extract_data()
+    export_to_db("oscars_history", df_oscars)
 
-    df = pd.DataFrame(oscar_data)
-
-    print(len(df))
+    print(len(df_oscars))
 
 if __name__ == "__main__":
     main()
