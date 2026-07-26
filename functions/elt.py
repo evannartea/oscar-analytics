c7fa2129c1b28b6b3f3692b1cae9a7cbcdd4ae8f
Evan Nartea <evannartea@Mac.lan>
2026-07-26 13:04:19 +0100

    added multiple endpoints

--- functions/elt.py
+++ functions/elt.py
@@ -1,9 +1,9 @@
 import requests
 import pandas as pd
 
-def extract_data():
+def extract_data(endpoint):
     # Define API endpoint
-    url = "https://api.oscarbase.com/api/nominations"
+    url = f"https://api.oscarbase.com/api/{endpoint}"
 
     # Send GET request to API endpoint
     response = requests.get(url)
@@ -11,26 +11,59 @@ def extract_data():
     # Parse response content as JSON
     data = response.json()
 
-    # Create empty list to contain data
-    nominations = []
+    if endpoint == "nominations":
+        # Create empty list to store response content
+        nominations = []
 
-    # Append data from API endpoint to empty list
-    for nomination in data["data"]:
-        nominations.append({
-            "nomination_id": nomination["id"],
-            "ceremony_id": nomination["ceremony_id"],
-            "ceremony_year": nomination["ceremony_year"],
-            "category_id": nomination["category_id"],
-            "category": nomination["category"],
-            "movie_id": nomination["movie_id"],
-            "movie": nomination["movie"],
-            "nominee_id": nomination["nominee_id"],
-            "nominee": nomination["nominee"],
-            "is_song": nomination["is_song"],
-            "winner": nomination["winner"]
-        })
+        # Append data from API endpoints to empty list
+        for nomination in data["data"]:
+            nominations.append({
+                "nomination_id": nomination["id"],
+                "ceremony_id": nomination["ceremony_id"],
+                "ceremony_year": nomination["ceremony_year"],
+                "category_id": nomination["category_id"],
+                "category": nomination["category"],
+                "movie_id": nomination["movie_id"],
+                "movie": nomination["movie"],
+                "nominee_id": nomination["nominee_id"],
+                "nominee": nomination["nominee"],
+                "is_song": nomination["is_song"],
+                "winner": nomination["winner"]
+            })
 
-    return pd.DataFrame(nominations)
+        return pd.DataFrame(nominations)
+
+    elif endpoint == "movies":
+        movies = []
+
+        for movie in data["data"]:
+            movies.append({
+                "movie_id": movie["id"],
+                "title": movie["title"],
+                "release_date": movie["release_date"],
+                "runtime": movie["runtime"],
+                "genres": movie["genres"],
+                "origin_country": movie["origin_country"]
+            })
+
+        return pd.DataFrame(movies)
+
+    elif endpoint == "nominees":
+        nominees = []
+
+        for nominee in data["data"]:
+            nominees.append({
+                "nominee_id": nominee["id"],
+                "full_name": nominee["name"],
+                "birthday": nominee["birthday"],
+                "deathday": nominee["deathday"],
+                "birthplace": nominee["place_of_birth"]
+            })
+
+        return pd.DataFrame(nominees)
+
+    else:
+        raise ValueError("Invalid endpoint!")
 
 # Export DataFrame to PostgreSQL database
 def export_to_db(table_name, df, engine):
