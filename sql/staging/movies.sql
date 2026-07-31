6ff83d1ef755dbb2bd7993315ca5bf22667415e9
Evan Nartea <evannartea@Mac.lan>
2026-07-31 15:07:00 +0100

    update tables

--- sql/staging/movies.sql
+++ sql/staging/movies.sql
@@ -2,17 +2,20 @@ CREATE TABLE staging.movies AS
 SELECT DISTINCT
 	rm.movie_id,
 	rm.title,
-	CASE
-		WHEN rm.movie_id = 3943 THEN date '1946-03-18'
-		WHEN rm.movie_id = 5581 THEN date '1976-08-10'
-		WHEN rm.movie_id = 6095 THEN date '1987-02-25'
-		WHEN rm.movie_id = 6360 THEN date '1991-08-22'
-		WHEN rm.movie_id = 7575 THEN date '2013-09-20'
-		WHEN rm.movie_id = 8078 THEN date '2023-09-23'
-		ELSE rm.release_date::date
-	END AS release_date,
+	rm.release_date,
     rm.runtime,
 	rm.genres,
 	rm.origin_country
 FROM raw.movies rm
 ORDER BY rm.movie_id;
+
+UPDATE staging.movies sm
+SET release_date = CASE
+	WHEN sm.movie_id = 3943 THEN date '1946-03-18'
+	WHEN sm.movie_id = 5581 THEN date '1976-08-10'
+	WHEN sm.movie_id = 6095 THEN date '1987-02-25'
+	WHEN sm.movie_id = 6360 THEN date '1991-08-22'
+	WHEN sm.movie_id = 7575 THEN date '2013-09-20'
+	WHEN sm.movie_id = 8078 THEN date '2023-09-23'
+END
+WHERE sm.movie_id in (3943, 5581, 6095, 6360, 7575, 8078);
--- sql/staging/nominees.sql
+++ sql/staging/nominees.sql
@@ -8,3 +8,15 @@ SELECT
 	rne.birthplace
 FROM raw.nominees rne
 ORDER BY rne.full_name;
+
+UPDATE staging.nominees sne
+SET birthplace = CASE
+	WHEN sne.nominee_id = 146 THEN 'Sándorháza, Austria-Hungary [now Romania]'
+	WHEN sne.nominee_id = 228 THEN 'Drohobych, Austro-Hungarian Empire [now Ukraine]'	
+	WHEN sne.nominee_id = 539 THEN 'Sucha, Galicia, Austria-Hungary [now Poland]'
+	WHEN sne.nominee_id = 874 THEN 'Wiznitz, Bukovina, Austria-Hungary [now Ukraine]'
+	WHEN sne.nominee_id = 1297 THEN 'Sopron, Austria-Hungary [now Hungary]'
+	WHEN sne.nominee_id = 2022 THEN 'Vienna, Austria-Hungary [now Austria]'
+	WHEN sne.nominee_id = 2848 THEN 'Budzanów, Austria-Hungary [now Ukraine]'
+END
+WHERE sne.nominee_id in (146, 228, 539, 874, 1297, 2022, 2848);
