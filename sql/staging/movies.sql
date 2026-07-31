782654b0ac296b8c72e44b0f7945c89cba1ca38e
Evan Nartea <evannartea@Mac.lan>
2026-07-31 12:41:15 +0100

    fixed null release_dates

--- sql/staging/movies.sql
+++ sql/staging/movies.sql
@@ -2,7 +2,15 @@ CREATE TABLE staging.movies AS
 SELECT DISTINCT
 	rm.movie_id,
 	rm.title,
-	rm.release_date::date AS release_date,
+	CASE
+		WHEN rm.movie_id = 3943 THEN date '1946-03-18'
+		WHEN rm.movie_id = 5581 THEN date '1976-08-10'
+		WHEN rm.movie_id = 6095 THEN date '1987-02-25'
+		WHEN rm.movie_id = 6360 THEN date '1991-08-22'
+		WHEN rm.movie_id = 7575 THEN date '2013-09-20'
+		WHEN rm.movie_id = 8078 THEN date '2023-09-23'
+		ELSE rm.release_date::date
+	END AS release_date,
     rm.runtime,
 	rm.genres,
 	rm.origin_country
