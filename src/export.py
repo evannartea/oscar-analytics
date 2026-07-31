8c2a9724550f76c96c73bfe5f04370af43532afc
Evan Nartea <evannartea@Mac.lan>
2026-07-31 16:55:54 +0100

    export dim tables

--- src/export.py
+++ src/export.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from config import engine
 
 # Export data from PostgreSQL to CSV files
 def export_to_csv(table_name, engine):
@@ -8,5 +9,17 @@ def export_to_csv(table_name, engine):
     df.to_csv(file_path, index=False)
 
     return file_path
+
+export_to_csv("bridge_movie_country", engine)
+export_to_csv("bridge_movie_genre", engine)
+export_to_csv("category_era", engine)
+export_to_csv("dim_category", engine)
+export_to_csv("dim_ceremony", engine)
+export_to_csv("dim_country", engine)
+export_to_csv("dim_genre", engine)
+export_to_csv("dim_movie", engine)
+export_to_csv("dim_movie_era", engine)
+export_to_csv("dim_nominee", engine)
+export_to_csv("fct_nods", engine)
 
 print("Exported successfully!")
