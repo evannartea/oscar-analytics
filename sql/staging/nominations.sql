dc4d0849f4a6d9ca2d2561b0353072e4273df31d
Evan Nartea <evannartea@Mac.lan>
2026-08-03 11:16:32 +0100

    changed id to int type

--- sql/staging/nominations.sql
+++ sql/staging/nominations.sql
@@ -3,8 +3,8 @@ SELECT DISTINCT
 	rn.nomination_id,
 	rn.category_id,
 	rn.ceremony_id,
-	rn.movie_id,
-	rn.nominee_id,
+	rn.movie_id::BIGINT as movie_id,
+	rn.nominee_id::BIGINT as nominee_id,
 	rn.is_song,
 	rn.winner
 FROM raw.nominations rn
